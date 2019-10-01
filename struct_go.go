c52b260f316b2c9222ce53e008da63fee4b6cc60
Lalu Kismara Hadi <n.kismara@gmail.com>
2019-10-01 21:42:53 +0700

    Belajar Struct

---
+++ struct_go.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Car struct {
+	Merek string
+	Tahun string
+	Harga string
+}
+
+func (c2 Car) Get() {
+	fmt.Println(c2)
+}
+func main() {
+	var c Car
+	c = Car{
+		Merek: "oke",
+		Tahun: "2019",
+		Harga: "Rp 190000",
+	}
+	c.Get()
+	c1 := Car{
+		Merek: "oce",
+		Tahun: "2020",
+		Harga: "Rp 330000",
+	}
+	c1.Get()
+}
